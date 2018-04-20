30db63960a2b5991445924c81cb8119dd3209828
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-20 18:09:17 +0000

    Update InputController class

--- include/tiefblau/InputController.hpp
+++ include/tiefblau/InputController.hpp
@@ -2,10 +2,22 @@
 
 #pragma once
 
+struct iInputImpl;
+
 class cInputController final
 {
 public:
-	cInputController();
+	cInputController(iInputImpl *apImpl) : mpImpl(apImpl){}
 	~cInputController();
+	
+	/// @return true if specified key was pressed previously and still pressed
+	bool IsKeyDown(int anKey) const;
+	
+	/// @return true if specified key was pressed this frame
+	bool KeyWasPressed(int anKey) const;
+	
+	/// @return true if specified key was released this frame
+	bool KeyWasReleased(int anKey) const;
 private:
+	iInputImpl *mpImpl{nullptr};
 };
